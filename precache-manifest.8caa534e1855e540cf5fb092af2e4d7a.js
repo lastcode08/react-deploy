46b80a8852112d13e2e731671d53eaaeab5a6f88
Nasrul Fuad <nasrulfuad08@gmail.com>
2020-01-21 21:23:34 +0700

    Updates

---
+++ precache-manifest.8caa534e1855e540cf5fb092af2e4d7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccef9aa006f119c9507c6b2517bc7593",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "a575a95b58bde4de7bf3",
+    "url": "/react-deploy/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/react-deploy/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "a575a95b58bde4de7bf3",
+    "url": "/react-deploy/static/js/main.42434f4d.chunk.js"
+  },
+  {
+    "revision": "4bbd4cc65473aff00d33",
+    "url": "/react-deploy/static/js/runtime~main.82fe89cb.js"
+  }
+]);
